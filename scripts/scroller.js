de4c2c89823001516cc9372b00079bc2b549cad2
TyMoOteUuSz <tymooteuusz@gmail.com>
2019-08-05 21:52:53 +0200

    Added responsive buttons

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -1,20 +1,20 @@
 $(document).ready(function (){
-    $(".about_nav").click(function (){
+    $(".about_click").click(function (){
         $('html, body').animate({
             scrollTop: $(".about").offset().top
         }, 1000);
     });
-    $(".skills_nav").click(function (){
+    $(".skills_click").click(function (){
         $('html, body').animate({
             scrollTop: $(".skills").offset().top
         }, 1000);
     });
-    $(".portfolio_nav").click(function (){
+    $(".portfolio_click").click(function (){
         $('html, body').animate({
             scrollTop: $(".portfolio").offset().top
         }, 1000);
     });
-    $(".contact_nav").click(function (){
+    $(".contact_click").click(function (){
         $('html, body').animate({
             scrollTop: $(".contact").offset().top
         }, 1000);
